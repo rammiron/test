9f4d6e192a40255a1416bd0d991cfaa5c65108fd
rammiron <rammiron666@gmail.com>
2024-12-18 21:24:43 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double V10;
+            double x = Convert.ToDouble(Input.Text);
+
+            V10 = (Math.Exp(x) * Math.Tan(Math.Pow(Math.Pow(x, 2) + 5.3, 0.333)) + Math.Pow(x, 5)) / (1 + Math.Log(Math.Abs(x)));
+            string v = Convert.ToString(V10);
+            Output.Text = v;
+        }
+    }
+}
